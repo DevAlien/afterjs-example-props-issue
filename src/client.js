74036e9312afea132d968bfe46a774cb28f80f1f
Goncalo Margalho <gsky89@gmail.com>
2020-02-22 17:38:42 +0100

    first commit

---
+++ src/client.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { hydrate } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ensureReady, After } from "@jaredpalmer/after";
+import routes from "./routes";
+import "./client.css";
+import { Provider } from "react-redux";
+import configureStore from "./common/store/configureStore";
+
+const preloadedState = window.__PRELOADED_STATE__;
+delete window.__PRELOADED_STATE__;
+const store = configureStore(preloadedState);
+
+function Main({ data }) {
+  console.log("data", data);
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <After data={data} routes={routes} store={store} />
+      </Provider>
+    </BrowserRouter>
+  );
+}
+function renderApp() {
+  ensureReady(routes).then(data =>
+    hydrate(<Main data={data} />, document.getElementById("root"))
+  );
+}
+
+renderApp();
+
+if (module.hot) {
+  module.hot.accept("./routes", renderApp);
+}
